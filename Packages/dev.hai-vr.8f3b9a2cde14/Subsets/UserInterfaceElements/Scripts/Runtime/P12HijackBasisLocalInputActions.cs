2472c92a0d5f721c932b3d0b48eed3e1a9b3b34b
Haï~ <hai-vr@users.noreply.github.com>
2025-06-04 05:56:17 +0200

    wip

--- Packages/dev.hai-vr.8f3b9a2cde14/Subsets/UserInterfaceElements/Scripts/Runtime/P12HijackBasisLocalInputActions.cs
+++ Packages/dev.hai-vr.8f3b9a2cde14/Subsets/UserInterfaceElements/Scripts/Runtime/P12HijackBasisLocalInputActions.cs
@@ -1,5 +1,6 @@
 ﻿using Basis.Scripts.Device_Management.Devices.Desktop;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Hai.Project12.UserInterfaceElements
 {
@@ -7,8 +8,25 @@ namespace Hai.Project12.UserInterfaceElements
     {
         private void OnEnable()
         {
+            var instance = BasisLocalInputActions.Instance;
+            instance.RightMousePressed.action.performed -= OnRightMouse;
+            instance.RightMousePressed.action.performed += OnRightMouse;
+            instance.RightMousePressed.action.canceled -= OnRightMouse;
+            instance.RightMousePressed.action.canceled += OnRightMouse;
             // BasisLocalInputActions blia = null;
             // blia.Escape.action.performed += BasisLocalInputActions.OnEscapePerformed;
+        }
+
+        private void OnDisable()
+        {
+            var instance = BasisLocalInputActions.Instance;
+            instance.RightMousePressed.action.performed -= OnRightMouse;
+            instance.RightMousePressed.action.canceled -= OnRightMouse;
+        }
+
+        private void OnRightMouse(InputAction.CallbackContext ctx)
+        {
+            BasisLocalInputActions.Instance.InputState.SecondaryTrigger = ctx.ReadValue<float>();
         }
     }
 }
