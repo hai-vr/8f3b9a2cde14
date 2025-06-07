cc0aa5b4746b2a213bc797b43c7e655b0c99ddc3
Haï~ <hai-vr@users.noreply.github.com>
2025-06-07 19:44:33 +0200

    Prepare to expand VixxyControl Property editor.

--- Packages/dev.hai-vr.8f3b9a2cde14/Subsets/Vixxy/Scripts/Editor/P12VixxyControlEditor.cs
+++ Packages/dev.hai-vr.8f3b9a2cde14/Subsets/Vixxy/Scripts/Editor/P12VixxyControlEditor.cs
@@ -55,33 +55,8 @@ namespace Subsets.Vixxy.Scripts.Editor
                 EditorGUILayout.LabelField($"Properties array size is {propertiesSp.arraySize}");
                 for (var propertyIndex = 0; propertyIndex < propertiesSp.arraySize; propertyIndex++)
                 {
-                    EditorGUILayout.BeginVertical("GroupBox");
-                    EditorGUILayout.BeginHorizontal();
                     var propertySp = propertiesSp.GetArrayElementAtIndex(propertyIndex);
-                    var genericType = propertySp.managedReferenceValue.GetType().GetGenericArguments()[0];
-                    if (propertySp.managedReferenceValue is P12VixxyPropertyBase)
-                    {
-                        EditorGUILayout.LabelField($"[{propertyIndex}] Property of type {genericType.Name}", EditorStyles.boldLabel);
-                    }
-                    else
-                    {
-                        EditorGUILayout.LabelField($"[{propertyIndex}] CAUTION: Not a P12VixxyPropertyBase, type is {propertySp.managedReferenceValue.GetType().FullName}", EditorStyles.boldLabel);
-                    }
-                    if (HaiEFCommon.ColoredBackground(true, Color.red, () => GUILayout.Button($"{CrossSymbol}", GUILayout.Width(DeleteButtonWidth))))
-                    {
-                        propertiesSp.DeleteArrayElementAtIndex(propertyIndex);
-
-                        serializedObject.ApplyModifiedProperties();
-                        return; // Workaround array size change error
-                    }
-                    EditorGUILayout.EndHorizontal();
-
-                    EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyPropertyBase.fullClassName)));
-                    EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyPropertyBase.propertyName)));
-                    EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyPropertyBase.flip)));
-                    EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyProperty<object>.unbound)));
-                    EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyProperty<object>.bound)));
-                    EditorGUILayout.EndVertical();
+                    if (DrawPropertyOrReturn(propertySp, propertyIndex, propertiesSp)) return;
                 }
 
                 if (GUILayout.Button("+ Add Property of type FLOAT"))
@@ -107,6 +82,37 @@ namespace Subsets.Vixxy.Scripts.Editor
             serializedObject.ApplyModifiedProperties();
 
             DrawDefaultInspector();
+        }
+
+        private bool DrawPropertyOrReturn(SerializedProperty propertySp, int propertyIndex, SerializedProperty propertiesSp)
+        {
+            EditorGUILayout.BeginVertical("GroupBox");
+            EditorGUILayout.BeginHorizontal();
+            var genericType = propertySp.managedReferenceValue.GetType().GetGenericArguments()[0];
+            if (propertySp.managedReferenceValue is P12VixxyPropertyBase)
+            {
+                EditorGUILayout.LabelField($"[{propertyIndex}] Property of type {genericType.Name}", EditorStyles.boldLabel);
+            }
+            else
+            {
+                EditorGUILayout.LabelField($"[{propertyIndex}] CAUTION: Not a P12VixxyPropertyBase, type is {propertySp.managedReferenceValue.GetType().FullName}", EditorStyles.boldLabel);
+            }
+            if (HaiEFCommon.ColoredBackground(true, Color.red, () => GUILayout.Button($"{CrossSymbol}", GUILayout.Width(DeleteButtonWidth))))
+            {
+                propertiesSp.DeleteArrayElementAtIndex(propertyIndex);
+
+                serializedObject.ApplyModifiedProperties();
+                return true; // Workaround array size change error
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyPropertyBase.fullClassName)));
+            EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyPropertyBase.propertyName)));
+            EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyPropertyBase.flip)));
+            EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyProperty<object>.unbound)));
+            EditorGUILayout.PropertyField(propertySp.FindPropertyRelative(nameof(P12VixxyProperty<object>.bound)));
+            EditorGUILayout.EndVertical();
+            return false;
         }
     }
 }
