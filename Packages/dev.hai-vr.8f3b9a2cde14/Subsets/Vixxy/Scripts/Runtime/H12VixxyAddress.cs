b7b0172f0bab769e71d9cd67406d3a77d53d6b1b
Haï~ <hai-vr@users.noreply.github.com>
2025-06-07 19:43:49 +0200

    Prepare VixxyAddress for addresses that aren't tied to a manually edited system.

--- Packages/dev.hai-vr.8f3b9a2cde14/Subsets/Vixxy/Scripts/Runtime/H12VixxyAddress.cs
+++ Packages/dev.hai-vr.8f3b9a2cde14/Subsets/Vixxy/Scripts/Runtime/H12VixxyAddress.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEditor;
 
 namespace Hai.Project12.Vixxy.Runtime
 {
     public class H12VixxyAddress
     {
         private static readonly Dictionary<string, int> AddressToIdDict = new();
-        private static readonly Dictionary<int, string> IdToAddressDict = new(); // TODO: Could probably make a List and stop using _nextId
+        private static readonly Dictionary<int, string> IdToAddressDict = new(); // TODO: Could probably make a List and stop using _nextId, or make a bidirectional dictionary
         private static int _nextId = 1;
 
+        /// Generates a GUID address. Use this is when the string address doesn't matter, and you need an internal identifier to reference a value.
+        /// Please store this address, don't call this over and over.
+        /// Valid IDs start at 1.
+        public static int NewRandomAddress()
+        {
+            return AddressToId(GUID.Generate().ToString());
+        }
+
+        /// Returns an ID for that address, storing that address if it was not seen before.
+        /// This is ID only valid for the duration of the app's execution, don't store it across app executions.
+        /// Valid IDs start at 1.
         public static int AddressToId(string address)
         {
             if (AddressToIdDict.TryGetValue(address, out var id)) return id;
@@ -21,6 +33,7 @@ namespace Hai.Project12.Vixxy.Runtime
             return newId;
         }
 
+        /// Returns the string address for an ID that was returned by any method of this class. Throws an exception if that ID was never seen.
         public static string ResolveKnownAddressFromId(int iddress)
         {
             if (IdToAddressDict.TryGetValue(iddress, out var id)) return id;
